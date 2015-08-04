c2c4de979df24f5254941ed251800298f0d021cc
ethanhinson <ethan@bluetent.com>
2015-08-04 16:34:45 -0600

    Commeents

--- classes/image-optimize-jpg.php
+++ classes/image-optimize-jpg.php
@@ -6,10 +6,17 @@ class ImageOptimizeJpgHandler extends ImageOptimizeAbstractHandler {
   public $mime_type = 'image/jpeg';
   public $handler_slug = 'image_optimize_jpg';
 
+  /**
+   * Required to properly setup the object.
+   */
   public function __construct() {
     parent::__construct($this->handler_slug, $this->mime_type);
   }
 
+  /**
+   * Required to process any sort of handling you might need.
+   * @param $id
+   */
   public function attachment_optimize($id) {
     $images = self::get_images($id);
     $cmd = "cd " . escapeshellarg(ABSPATH) . "; ";
