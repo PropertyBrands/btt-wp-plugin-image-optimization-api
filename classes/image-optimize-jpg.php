d7afeeaccd3c67614659fb38605eeb6765042863
ethanhinson <ethan@bluetent.com>
2015-08-04 17:23:48 -0600

    finally fix shell command spacing

--- classes/image-optimize-jpg.php
+++ classes/image-optimize-jpg.php
@@ -10,7 +10,7 @@ class ImageOptimizeJpgHandler extends ImageOptimizeAbstractHandler {
    * Required to properly setup the object.
    */
   public function __construct() {
-    parent::__construct($this->handler_slug, $this->mime_type);
+    parent::__construct($this->handler_slug, $this->mime_type, get_option($this->handler_slug . '_binary'));
   }
 
   /**
@@ -22,11 +22,12 @@ class ImageOptimizeJpgHandler extends ImageOptimizeAbstractHandler {
     $cmd = "cd " . escapeshellarg(ABSPATH) . "; ";
     if($images) {
       foreach($images as $img) {
-        $cmd .= $this->$binary_path . "-p -m70 --strip-all" . escapeshellarg($img);
+        $cmd .= $this->binary_path . " -p -m70 --strip-all " . escapeshellarg($img) . "; ";
       }
+      $cmd .= ' 2>&1';
     }
     if(!empty($cmd)) {
-      shell_exec($cmd);
+      $output = shell_exec($cmd);
       //@todo: something with the output...
     }
   }
